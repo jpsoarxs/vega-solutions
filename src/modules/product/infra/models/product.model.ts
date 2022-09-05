4b54866288509186a8030289878f7f505b32e247
Joao Pedro Soares <jpsoarxs@gmail.com>
2022-09-05 19:07:05 -0300

    added product modell

---
+++ src/modules/product/infra/models/product.model.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm';
+import { ProductActions } from '../../entities/enum/product-actions.enum';
+import { ProductCategory } from '../../entities/enum/product-category.enum';
+
+@Entity('products')
+export class CustomerModel {
+	@PrimaryColumn('uuid')
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	description: string;
+
+	@Column()
+	price: number;
+
+	@Column({ type: 'enum', enum: ProductCategory })
+	category: ProductCategory;
+
+	@Column({ type: 'enum', enum: ProductActions })
+	action: ProductActions;
+
+	@Column()
+	created_at: Date;
+
+	@Column()
+	updated_at: Date;
+
+	@Column({ nullable: true })
+	deleted_at?: Date;
+}
